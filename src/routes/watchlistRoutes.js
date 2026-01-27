4cbe1bfb1bce66a172e2c3f727d033fedec17597
EdmarLelis <edmarlelis.lourenco@gmail.com>
2026-01-26 21:28:07 -0300

    feat: add Zod validation and request validation middleware

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -5,13 +5,20 @@ import {
   updateWatchlist,
 } from '../controllers/watchlistController.js';
 import { authMiddleeare } from '../middleware/authMiddleware.js';
+import { validateRequest } from '../middleware/validateRequest.js';
+import {
+  addToWatchlistSchema,
+  updateWatchlistSchema,
+} from '../validators/watchlistValidators.js';
 
 const router = express.Router();
 
 router.use(authMiddleeare);
 
-router.post('/', addToWatchlist);
+router.post('/', validateRequest(addToWatchlistSchema), addToWatchlist);
+
 router.delete('/:id', removeFromWatchlist);
-router.put('/:id', updateWatchlist);
+
+router.put('/:id', validateRequest(updateWatchlistSchema), updateWatchlist);
 
 export default router;
